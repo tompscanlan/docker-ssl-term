a194122e440ddb3ea83ca033b3b2b70ebfa8e18f
Tom Scanlan <tompscanlan@gmail.com>
2014-04-10 20:58:29 -0400

    retab and fix heatbleed

--- Dockerfile
+++ Dockerfile
@@ -8,19 +8,20 @@
 # to run server: 
 #
 # Pull base image.
-FROM dockerfile/ubuntu
+#FROM dockerfile/ubuntu
+FROM ubuntu
 MAINTAINER Tom Scanlan, tscanlan@momentumsi.com
 
 # Install Nginx.
-RUN apt-get install -y software-properties-common
+ENV LAST_UPDATED 2014-4-9
+RUN apt-get update
+RUN apt-get install -y software-properties-common python-software-properties
 RUN add-apt-repository -y ppa:nginx/stable
 RUN apt-get update
+RUN apt-get install -y libssl1.0.0
 RUN apt-get install -y nginx
 RUN echo "\ndaemon off;" >> /etc/nginx/nginx.conf
 
-
-ADD . /src
-RUN cd /src/ 
 # Attach volumes.
 VOLUME /etc/nginx/sites-enabled
 VOLUME /etc/nginx/conf.d
@@ -32,7 +33,7 @@ WORKDIR /etc/nginx
 # Expose ports.
 #EXPOSE 80
 EXPOSE 443
-#EXPOSE 444
+EXPOSE 444
 
 # Define default command.
 ENTRYPOINT ["nginx"]
--- common.sh
+++ common.sh
@@ -2,18 +2,18 @@
 
 add_server_confg() {
 	mkdir -p ./logs/$1
-	cat > ./sites-enabled/$1 << EOF  
+	cat > ./sites-enabled/$1 << EOF
 server {
 	### server port and name ###
-	listen          $3;
-	server_name     $1;
-	ssl 		on;
+	listen		$3;
+	server_name	$1;
+	ssl		on;
 
 	### SSL log files ###
-	access_log      /var/log/nginx/$1/access.log;
-	error_log       /var/log/nginx/$1/error_log;
+	access_log	/var/log/nginx/$1/access.log;
+	error_log	/var/log/nginx/$1/error_log;
 
-	# move the to the http section if we're using same 
+	# move the to the http section if we're using same
 	# certs across all sites.  more eff. memory wise
 	### SSL cert files ###
 	ssl_certificate      conf.d/ssl/momentum.in.crt;
@@ -22,7 +22,7 @@ server {
 	### Add SSL specific settings here ###
 
 
-	ssl_protocols        SSLv3 TLSv1 TLSv1.1 TLSv1.2;
+	ssl_protocols	     SSLv3 TLSv1 TLSv1.1 TLSv1.2;
 	ssl_ciphers RC4:HIGH:!aNULL:!MD5;
 	ssl_prefer_server_ciphers on;
 	keepalive_timeout    60;
@@ -37,20 +37,21 @@ server {
 		proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
 
 		### Set headers ####
-		proxy_set_header        Accept-Encoding   "";
-		proxy_set_header        Host            \$host;
-		proxy_set_header        X-Real-IP       \$remote_addr;
-		proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+		proxy_set_header	Accept-Encoding   "";
+		proxy_set_header	Host		\$host;
+		proxy_set_header	X-Real-IP	\$remote_addr;
+		proxy_set_header	X-Forwarded-For \$proxy_add_x_forwarded_for;
 
 		### Most PHP, Python, Rails, Java App can use this header ###
 		#proxy_set_header X-Forwarded-Proto https;##
 		#This is better##
-		proxy_set_header        X-Forwarded-Proto \$scheme;
-			add_header              Front-End-Https   on;
+		proxy_set_header	X-Forwarded-Proto \$scheme;
+			add_header		Front-End-Https   on;
 
 
 		### By default we don't want to redirect it ####
-		proxy_redirect     off;
+		# Jenkins needs this; it redirects after login
+		proxy_redirect http:// https://;
 	}
 }
 EOF
